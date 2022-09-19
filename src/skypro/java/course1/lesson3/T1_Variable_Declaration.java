eb5dda1b3c41405402ad09de89c4b8963fd4b661
fdsnk <fedoseenkoms@gmail.com>
2022-09-19 23:51:22 +0300

    Homework for lesson 3 - Variables is completed

---
+++ src/skypro/java/course1/lesson3/T1_Variable_Declaration.java
@@ -0,0 +1,15 @@
+package skypro.java.course1.lesson3;
+/*
+Присвоить переменным разные значения.
+Для решения этой задачи используйте тип переменной var.
+ */
+public class T1_Variable_Declaration {
+    public static void main(String[] args) {
+        var dog = 6;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+    }
+}
