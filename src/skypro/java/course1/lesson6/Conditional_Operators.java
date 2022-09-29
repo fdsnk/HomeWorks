d72da7b687057f360b9adb790f66e8808fe4b4c1
fdsnk <fedoseenkoms@gmail.com>
2022-09-30 00:14:37 +0300

    4 task completed. 1 and 2 tasks corrected

--- src/skypro/java/course1/lesson6/Conditional_Operators.java
+++ src/skypro/java/course1/lesson6/Conditional_Operators.java
@@ -5,6 +5,7 @@ public class Conditional_Operators {
         task1_mobile_application();
         task2_mobile_application_light();
         task3_leap_years();
+        task4_delivery();
     }
 
     private static void task1_mobile_application() {
@@ -25,8 +26,8 @@ public class Conditional_Operators {
             case 1:
                 System.out.println("Установите версию приложения для Android по ссылке.\n");
                 break;
-            default:
-                System.out.println("К сожалению, для Вашего устройства нет приложения.\n");
+            //    default:
+            //        System.out.println("К сожалению, для Вашего устройства нет приложения.\n");  //
         }
     }
 
@@ -46,7 +47,7 @@ public class Conditional_Operators {
         Для обозначения года создания используйте переменную clientDeviceYear, где необходимо указать 2015 год.
         **Важно**: вложенность больше двух этажей не допускается (условный оператор внутри условного оператора).
          */
-        int clientOS = 3; // 0 — iOS, 1 — Android
+        int clientOS = 1; // 0 — iOS, 1 — Android
         int clientDeviceYear = 2014;
         if (clientDeviceYear >= 2015 && clientOS == 0) {
             System.out.println("Установите обычную версию приложения для iOS по ссылке.\n");
@@ -54,9 +55,9 @@ public class Conditional_Operators {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке.\n");
         } else if (clientDeviceYear >= 2015 && clientOS == 1) {
             System.out.println("Установите обычную версию приложения для Android по ссылке.\n");
-        } else if (clientDeviceYear < 2015 && clientOS == 1) {
+        } else {
             System.out.println("Установите облегченную версию приложения для Android по ссылке.\n");
-        } else System.out.println("К сожалению, для Вашего устройства нет приложения.\n");
+        }
     }
 
     public static void task3_leap_years() {
@@ -72,12 +73,41 @@ public class Conditional_Operators {
         Небольшая справка: високосным является каждый четвертый год, но не является каждый сотый.
         Также високосным является каждый четырехсотый год.
          */
-        int year = 2000;
+        int year = 2100;
         boolean leapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
         if (leapYear) {
             System.out.println(year + " год является високосным.\n");
         } else {
             System.out.println(year + " год не является високосным.\n");
+        }
+    }
+
+    public static void task4_delivery() {
+        System.out.println("Задание 4.\n");
+        /*
+        Чтобы известить клиента о том, когда будет доставлена его карта,
+        нам нужно знать расстояние от нашего офиса до адреса доставки.
+        Правила доставки такие:
+        Доставка в пределах 20 км занимает сутки.
+        Доставка в пределах от 20 км до 60 км добавляет еще один день доставки.
+        Доставка в пределах 60 км до 100 км добавляет еще одни сутки.
+        То есть с каждым следующим интервалом доставки срок увеличивается на 1 день.
+        Напишите программу, которая выдает сообщение в консоль:
+        "Потребуется дней: " + срок доставки
+        Объявите целочисленную переменную deliveryDistance = 95, которая содержит дистанцию до клиента.
+        */
+
+        int deliveryDistance = 100;
+        int deliveryDays = 1;
+        int distanceAddDays = 40; // Шаг расстояния , при котором увеличивается доставка на СУТКИ
+        int remainingDistance = deliveryDistance - 20; // Если разница <= 0 , то выведем СУТКИ на доставку, если больше, то ИНАЧЕ
+        System.out.print("Потребуется дней: ");
+        if (remainingDistance <= 0) {
+            System.out.println(deliveryDays);
+        } else if (remainingDistance % distanceAddDays != 0) {
+            System.out.println(deliveryDays += remainingDistance / distanceAddDays + 1);
+        } else {
+            System.out.println(deliveryDays += remainingDistance / distanceAddDays);
         }
     }
 }
