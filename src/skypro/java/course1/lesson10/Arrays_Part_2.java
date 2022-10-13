0cfa1139bccfd235378ae1222e7e103024ea643d
fdsnk <fedoseenkoms@gmail.com>
2022-10-13 16:00:49 +0300

    task 4 - fixed

--- src/skypro/java/course1/lesson10/Arrays_Part_2.java
+++ src/skypro/java/course1/lesson10/Arrays_Part_2.java
@@ -39,7 +39,7 @@ public class Arrays_Part_2 {
 
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int c = reverseFullName.length - 1; c >= 0; c--) {
-            System.out.print(reverseFullName[c] + " ");
+            System.out.print(reverseFullName[c]);
         }
     }
 
