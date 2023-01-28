ac66b2e4d5871e3d77905ca9c264b0f5eb9b6d9d
fdsnk <fedoseenkoms@gmail.com>
2023-01-28 10:58:53 +0300

    Coursework completed - Basic difficulty

---
+++ src/skypro/java/course2/coursework/task/tasks/WeeklyTask.java
@@ -0,0 +1,18 @@
+package skypro.java.course2.coursework.task.tasks;
+
+import skypro.java.course2.coursework.task.Repeatable;
+import skypro.java.course2.coursework.task.Task;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class WeeklyTask extends Task implements Repeatable {
+    public WeeklyTask(String title, String description, boolean isWork, LocalDateTime dateTime) {
+        super(title, description, isWork, dateTime);
+    }
+
+    public boolean checkIfSuitable(LocalDate date) {
+        return date.getDayOfWeek() == dateTime.getDayOfWeek()
+                && (date.equals(dateTime.toLocalDate()) || date.isAfter(dateTime.toLocalDate()));
+    }
+}
