2cd06ef001eaeb7abb8d63477ad67ae9c1525a8d
fdsnk <fedoseenkoms@gmail.com>
2023-06-12 23:11:17 +0300

    Practice - JUnit - Start

---
+++ src/test/java/pro/sky/course2/practice/lesson13/UniversityTest.java
@@ -0,0 +1,11 @@
+package pro.sky.course2.practice.lesson13;
+
+public class UniversityTest {
+
+    public void getAllStudents() {
+        Student student = new Student("Евгений", 35, true);
+        Student student2 = new Student("Евгений", 35, true);
+        Student student3 = new Student("Евгений", 35, true);
+
+    }
+}
